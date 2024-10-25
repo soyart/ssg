fd1967afc15a812a0732dee7b3f0da9994ddcda6
soyart <artdrawin@gmail.com>
2024-10-25 16:13:20 +0000

    comment

--- flake.nix
+++ flake.nix
@@ -60,6 +60,11 @@ rec {
           pname = "ssg";
           src = ./.;
           vendorHash = "sha256-fxD5o+7uC2lob86TPxlnqT5m7ZYVjIh9ZQANlVb4Pl4=";
+          # Go unit tests are already executed by buildGoModule.
+          # preBuild would instead be more useful if we want to set Go flags.
+          # preBuild = ''
+          #   go test ./...;
+          # '';
 
           meta = {
             homepage = "https://github.com/soyart/ssg";
