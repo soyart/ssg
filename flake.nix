b722e38540c08770f6cb0007d3989352598f6c36
soyart <artdrawin@gmail.com>
2024-11-09 15:52:18 +0000

    bump: go modules and nix vendor hash

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@ rec {
 
           pname = "ssg";
           src = ./.;
-          vendorHash = "sha256-fxD5o+7uC2lob86TPxlnqT5m7ZYVjIh9ZQANlVb4Pl4=";
+          vendorHash = "sha256-7uTw0JuVtwz6uUbNNPqGrUj9mwOtjcvr3NNwetlkUpo=";
           # Go unit tests are already executed by buildGoModule.
           # preBuild would instead be more useful if we want to set Go flags.
           # preBuild = ''
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.22.7
 
 require (
 	github.com/alexflint/go-arg v1.5.1
-	github.com/gomarkdown/markdown v0.0.0-20241102151059-6bc1ffdc6e8c
+	github.com/gomarkdown/markdown v0.0.0-20241105142532-d03b89096d81
 )
 
 require github.com/alexflint/go-scalar v1.2.0 // indirect
