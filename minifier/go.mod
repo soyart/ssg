d44b8967e3dfb98bf664aa8bc75467743904622f
Prem Phansuriyanon <prem.p@lmwn.com>
2024-11-21 23:56:28 +0700

    import minifier

--- minifier/go.mod
+++ minifier/go.mod
@@ -9,4 +9,8 @@ require github.com/soyart/ssg v0.0.0-00010101000000-000000000000
 require (
 	github.com/gomarkdown/markdown v0.0.0-20241105142532-d03b89096d81 // indirect
 	github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06 // indirect
+	github.com/tdewolff/minify v2.3.6+incompatible // indirect
+	github.com/tdewolff/minify/v2 v2.21.1 // indirect
+	github.com/tdewolff/parse v2.3.4+incompatible // indirect
+	github.com/tdewolff/parse/v2 v2.7.18 // indirect
 )
--- minifier/main.go
+++ minifier/main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+
 	"github.com/soyart/ssg"
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/html"
 )
 
 func main() {
-	fmt.Println("test visibility", string(ssg.ToHtml([]byte("Hello, world!"))))
+	const mediaType = "text/html"
+
+	header, err := os.ReadFile("../testdata/johndoe.com/src/_header.html")
+	if err != nil {
+		panic(err)
+	}
+	footer, err := os.ReadFile("../testdata/johndoe.com/src/_footer.html")
+	if err != nil {
+		panic(err)
+	}
+
+	buf := bytes.NewBuffer(header)
+
+	_, err = buf.Write(ssg.ToHtml([]byte("Hello, world!")))
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = buf.Write(footer)
+	if err != nil {
+		panic(err)
+	}
+
+	m := minify.New()
+	m.AddFunc(mediaType, html.Minify)
+
+	minified := bytes.NewBuffer(nil)
+	err = m.Minify(mediaType, minified, buf)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(minified.String())
 }
