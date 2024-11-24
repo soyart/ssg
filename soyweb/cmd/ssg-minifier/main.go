f6dd3cea83e84fcf6624052ed3db49c19f97d6c9
Prem Phansuriyanon <prem.p@lmwn.com>
2024-11-24 22:34:46 +0700

    ssg-minifier now accepts envs

--- soyweb/cmd/ssg-minifier/main.go
+++ soyweb/cmd/ssg-minifier/main.go
@@ -9,21 +9,79 @@ import (
 	"github.com/soyart/ssg/soyweb"
 )
 
+const (
+	yes string = "1"
+
+	envNoHtml    string = "NO_HTML"
+	envNoHtmlAll string = "NO_HTML_ALL"
+	envNoCss     string = "NO_CSS"
+	envNoJson    string = "NO_JSON"
+)
+
 func main() {
 	if len(os.Args) < 5 {
 		fmt.Fprint(os.Stdout, "usage: ssg src dst title base_url\n")
 		syscall.Exit(1)
 	}
 
-	src, dst, title, url := os.Args[1], os.Args[2], os.Args[3], os.Args[4]
-	s := ssg.NewWithOptions(src, dst, title, url,
-		ssg.ParallelWritesEnv(),
-		ssg.Pipeline(soyweb.MinifyAll),
-		ssg.Hook(soyweb.MinifyHtml),
+	f := negateFromEnvs(soyweb.MinifyFlags{
+		MinifyHtml:    true,
+		MinifyHtmlAll: true,
+		MinifyCss:     true,
+		MinifyJson:    true,
+	})
+
+	opts := append(
+		[]ssg.Option{ssg.ParallelWritesEnv()},
+		soyweb.SsgOptions(soyweb.Flags{MinifyFlags: f})...,
 	)
+
+	src, dst, title, url := os.Args[1], os.Args[2], os.Args[3], os.Args[4]
+	s := ssg.NewWithOptions(src, dst, title, url, opts...)
 
 	err := s.Generate()
 	if err != nil {
 		panic(err)
 	}
+}
+
+type envs struct {
+	noHtml    bool
+	noHtmlAll bool
+	noCss     bool
+	noJson    bool
+}
+
+func parseEnvs() envs {
+	result := envs{}
+	if os.Getenv(envNoHtml) == yes {
+		result.noHtml = true
+	}
+	if os.Getenv(envNoHtmlAll) == yes {
+		result.noHtmlAll = true
+	}
+	if os.Getenv(envNoCss) == yes {
+		result.noCss = true
+	}
+	if os.Getenv(envNoJson) == yes {
+		result.noJson = true
+	}
+	return result
+}
+
+func negateFromEnvs(f soyweb.MinifyFlags) soyweb.MinifyFlags {
+	e := parseEnvs()
+	if e.noHtml {
+		f.MinifyHtml = false
+	}
+	if e.noHtmlAll {
+		f.MinifyHtmlAll = false
+	}
+	if e.noCss {
+		f.MinifyCss = false
+	}
+	if e.noJson {
+		f.MinifyJson = false
+	}
+	return f
 }
